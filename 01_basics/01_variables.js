23a88bb7235b06379521a7d04aea07cb7f125f10
israfil <astro.israfil.fi@gmail.com>
2025-08-06 11:28:55 +0600

    learn variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const userId = 457845;
+let userEmail = "israfil@something.com";
+var password = "123";
+location = "dhaka";
+let state;
+
+// userId = 233434;     // TypeError: Assignment to a constant variable.
+
+userEmail = "israfil@microsoft.com";
+password = "123456";
+location = "California";
+
+console.log(state);
+
+/*
+    NOTE:
+            Always use const and let to declare a variable
+            Always try not to use var to declare a variable
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // from this line treat all js code as newer version;
+
+let firstName = "Israfil";
+let age = 24;
+let isLoggedIn = true;
+
+
+// number (up to 2^53 - 1)
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> a standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
